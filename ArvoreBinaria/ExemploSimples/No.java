86004039ee09284507e37695046f9aa6091475e9
Victoria Lacerda <victoria.simeao@soulasalle.com.br>
2025-06-30 15:42:08 -0300

    Create No.java

---
+++ ArvoreBinaria/ExemploSimples/No.java
@@ -0,0 +1,11 @@
+// Classe que representa um nó da árvore
+public class No {
+    int valor;
+    No esquerda, direita;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
